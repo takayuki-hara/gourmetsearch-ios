577b654ad82cd73886b8c3c10c3b63aa5d1ef0b9
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-07-31 16:59:35 +0900

    MyPage関連の画面と遷移を追加

--- gourmetsearch/Sources/Adaptors/ViewController/MyPage/MyPageViewController.swift
+++ gourmetsearch/Sources/Adaptors/ViewController/MyPage/MyPageViewController.swift
@@ -8,24 +8,60 @@
 
 import UIKit
 import SnapKit
+import XLPagerTabStrip
 
-class MyPageViewController: UIViewController {
+class MyPageViewController: SegmentedPagerTabStripViewController {
 
+    // MARK: - Properties
+    let scroll = UIScrollView()
+    let segmented = UISegmentedControl()
+
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
+        beforeViewDidLoad()
         super.viewDidLoad()
 
-        // for debug
-        let label = UILabel()
-        label.text = "MyPageViewController"
-        view.addSubview(label)
-        label.snp.makeConstraints { make in
-            make.center.equalTo(view)
-        }
+        view.setNeedsUpdateConstraints()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    override func updateViewConstraints() {
+        setupConstraints()
+        super.updateViewConstraints()
+    }
+
+    // MARK: - Public Methods
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let firstVC = HistoryViewController()
+        let secondVC = FavoriteViewController()
+        let childViewControllers: [UIViewController] = [firstVC, secondVC]
+        return childViewControllers
+    }
+
+    // MARK: - Private Methods
+    private func beforeViewDidLoad() {
+        // XLPagerTabStrip を使用するためにviewDidLoad()の前に行う処理
+        view.addSubview(segmented)
+        containerView = scroll
+        segmentedControl = segmented
+    }
+
+    private func setupConstraints() {
+        segmentedControl.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(CGSize(width: 300, height: 30))
+        }
+        scroll.snp.makeConstraints { make in
+            make.top.equalTo(segmented.snp.bottom).offset(4)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+
 }
