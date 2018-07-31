b04a6b2df3513354de02bc98176c09d2646f89b2
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-07-31 17:30:34 +0900

    Webビューの画面を追加

---
+++ gourmetsearch/Sources/Defines/Consts.swift
@@ -0,0 +1,16 @@
+//
+//  Consts.swift
+//  gourmetsearch
+//
+//  Created by 原隆幸 on 2018/07/31.
+//  Copyright © 2018年 Penguin. All rights reserved.
+//
+
+import Foundation
+
+struct Consts {
+    static let sampleUrl = "https://www.apple.com"
+    static let termsUrl = "https://www.google.co.jp/"
+    static let privacyUrl = "https://www.google.co.jp/"
+    static let guidelineUrl = "https://www.google.co.jp/"
+}
