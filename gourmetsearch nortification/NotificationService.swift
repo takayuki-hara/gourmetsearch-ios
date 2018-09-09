24354795e6a85f16aad8510e1cc74c5df5fb92d1
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-09-09 10:02:16 +0900

    Extensionのリファクタ（途中）

--- gourmetsearch nortification/NotificationService.swift
+++ gourmetsearch nortification/NotificationService.swift
@@ -30,28 +30,46 @@ class NotificationService: UNNotificationServiceExtension {
         modifiedNotificationContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
 
         // does the payload contains a remote URL to download or a local URL?
-        if let mediaUrlString = request.content.userInfo["image_url"] as? String {
-            // see if the media URL is for a local file  (i.e., file://movie.mp4)
-            guard let mediaUrl = URL(string: mediaUrlString) else {
-                // attempt to create a URL to a file in local storage
-                var useAlternateText: Bool = true
-                if mediaUrlString.isEmpty == false {
-                    let mediaUrlFilename:NSString = mediaUrlString as NSString
-                    let fileName = (mediaUrlFilename.lastPathComponent as NSString).deletingPathExtension
-                    let fileExtension = (mediaUrlFilename.lastPathComponent as NSString).pathExtension
+        guard let mediaUrlString = request.content.userInfo["image_url"] as? String else {
+            // no media URL found in the payload, just pass on the orginal payload
+            contentHandler(request.content)
+            return
+        }
+        // see if the media URL is for a local file  (i.e., file://movie.mp4)
+        guard let mediaUrl = URL(string: mediaUrlString) else {
+            modifyContentWithLocalFile(mediaUrlString: mediaUrlString)
+            contentHandler(modifiedNotificationContent!)
+            return
+        }
 
-                    // is it in the bundle?
-                    if let localMediaUrlPath = Bundle.main.path(forResource: fileName, ofType: fileExtension) {
-                        // is the URL a local file URL?
-                        let localMediaUrl = URL.init(fileURLWithPath: localMediaUrlPath)
-                        if localMediaUrl.isFileURL == true {
-                            // create an attachment with the local media
-                            let mediaAttachment: UNNotificationAttachment? = createMediaAttachment(localMediaUrl)
+        // if we have a URL, try to download media (i.e., https://media.giphy.com/media/3oz8xJBbCpzG9byZmU/giphy.gif)
+        if mediaUrl.isFileURL == false {
+            // create a session to handle downloading of the URL
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+
+            // start a download task to handle the download of the media
+            weak var weakSelf: NotificationService? = self
+            session.downloadTask(with: mediaUrl, completionHandler: {(_ location: URL?, _ response: URLResponse?, _ error: Error?) -> Void in
+                var useAlternateText: Bool = true
+                // if the download succeeded, save it locally and then make an attachment
+                if error == nil {
+                    let downloadResponse = response as! HTTPURLResponse
+                    if (downloadResponse.statusCode >= 200 && downloadResponse.statusCode <= 299) {
+                        // download was successful, attempt save the media file
+                        let localMediaUrl = URL.init(fileURLWithPath: location!.path + mediaUrl.lastPathComponent)
+
+                        // remove any existing file with the same name
+                        try? FileManager.default.removeItem(at: localMediaUrl)
+
+                        // move the downloaded file from the temporary location to a new file
+                        if ((try? FileManager.default.moveItem(at: location!, to: localMediaUrl)) != nil) {
+                            // create an attachment with the new file
+                            let mediaAttachment: UNNotificationAttachment? = weakSelf?.createMediaAttachment(localMediaUrl)
 
                             // if no problems creating the attachment, we can use it
                             if mediaAttachment != nil {
                                 // set the media to display in the notification
-                                modifiedNotificationContent?.attachments = [mediaAttachment!]
+                                weakSelf?.modifiedNotificationContent?.attachments = [mediaAttachment!]
 
                                 // everything is ok
                                 useAlternateText = false
@@ -61,89 +79,62 @@ class NotificationService: UNNotificationServiceExtension {
                 }
 
                 // if any problems creating the attachment, use the alternate text if provided
+                // alternative text to display if there are any issues loading the media URL
                 if (useAlternateText == true) {
-                    if let mediaAltText = request.content.userInfo["image_alt"] as? String {
-                        if mediaAltText.isEmpty == false {
-                            modifiedNotificationContent?.body = mediaAltText
-                        }
-                    }
+                    self.modifyContentAltText()
                 }
 
                 // tell the OS we are done and here is the new content
-                contentHandler(modifiedNotificationContent!)
-                return
-            }
-
-            // if we have a URL, try to download media (i.e., https://media.giphy.com/media/3oz8xJBbCpzG9byZmU/giphy.gif)
-            if mediaUrl.isFileURL == false {
-                // create a session to handle downloading of the URL
-                let session = URLSession(configuration: URLSessionConfiguration.default)
-
-                // start a download task to handle the download of the media
-                weak var weakSelf: NotificationService? = self
-                session.downloadTask(with: mediaUrl, completionHandler: {(_ location: URL?, _ response: URLResponse?, _ error: Error?) -> Void in
-                    var useAlternateText: Bool = true
-                    // if the download succeeded, save it locally and then make an attachment
-                    if error == nil {
-                        let downloadResponse = response as! HTTPURLResponse
-                        if (downloadResponse.statusCode >= 200 && downloadResponse.statusCode <= 299) {
-                            // download was successful, attempt save the media file
-                            let localMediaUrl = URL.init(fileURLWithPath: location!.path + mediaUrl.lastPathComponent)
-
-                            // remove any existing file with the same name
-                            try? FileManager.default.removeItem(at: localMediaUrl)
-
-                            // move the downloaded file from the temporary location to a new file
-                            if ((try? FileManager.default.moveItem(at: location!, to: localMediaUrl)) != nil) {
-                                // create an attachment with the new file
-                                let mediaAttachment: UNNotificationAttachment? = weakSelf?.createMediaAttachment(localMediaUrl)
-
-                                // if no problems creating the attachment, we can use it
-                                if mediaAttachment != nil {
-                                    // set the media to display in the notification
-                                    weakSelf?.modifiedNotificationContent?.attachments = [mediaAttachment!]
-
-                                    // everything is ok
-                                    useAlternateText = false
-                                }
-                            }
-                        }
-                    }
-
-                    // if any problems creating the attachment, use the alternate text if provided
-                    // alternative text to display if there are any issues loading the media URL
-                    if (useAlternateText == true) {
-                        if let mediaAltText = request.content.userInfo["image_alt"] as? String {
-                            if mediaAltText.isEmpty == false {
-                                weakSelf?.modifiedNotificationContent?.body = mediaAltText
-                            }
-                        }
-                    }
-
-                    // tell the OS we are done and here is the new content
-                    weakSelf?.contentHandler!((weakSelf?.modifiedNotificationContent)!)
-                }).resume()
-            }
-        }
-        else {
-            // no media URL found in the payload, just pass on the orginal payload
-            contentHandler(request.content)
-            return
+                weakSelf?.contentHandler!((weakSelf?.modifiedNotificationContent)!)
+            }).resume()
         }
     }
 
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        // we took too long to download the media URL, use the alternate text if provided
-        if let mediaAltText = modifiedNotificationContent?.userInfo["image_alt"] as? String {
-            // alternative text to display if there are any issues loading the media URL
-            if mediaAltText.isEmpty == false {
-                modifiedNotificationContent?.body = mediaAltText
+        // Extensionの処理は時間的な制約があり打ち切られる前にここが呼ばれる
+        modifyContentAltText()
+        contentHandler!(modifiedNotificationContent!)
+    }
+
+    private func modifyContentWithLocalFile(mediaUrlString: String) {
+        // attempt to create a URL to a file in local storage
+        var useAlternateText: Bool = true
+        if mediaUrlString.isEmpty == false {
+            let mediaUrlFilename:NSString = mediaUrlString as NSString
+            let fileName = (mediaUrlFilename.lastPathComponent as NSString).deletingPathExtension
+            let fileExtension = (mediaUrlFilename.lastPathComponent as NSString).pathExtension
+
+            // is it in the bundle?
+            if let localMediaUrlPath = Bundle.main.path(forResource: fileName, ofType: fileExtension) {
+                // is the URL a local file URL?
+                let localMediaUrl = URL.init(fileURLWithPath: localMediaUrlPath)
+                if localMediaUrl.isFileURL {
+                    // create an attachment with the local media
+                    let mediaAttachment: UNNotificationAttachment? = createMediaAttachment(localMediaUrl)
+
+                    // if no problems creating the attachment, we can use it
+                    if mediaAttachment != nil {
+                        // set the media to display in the notification
+                        modifiedNotificationContent?.attachments = [mediaAttachment!]
+
+                        // everything is ok
+                        useAlternateText = false
+                    }
+                }
             }
         }
 
-        // tell the OS we are done and here is the new content
-        contentHandler!(modifiedNotificationContent!)
+        // if any problems creating the attachment, use the alternate text if provided
+        if (useAlternateText == true) {
+            modifyContentAltText()
+        }
+    }
+
+    private func modifyContentAltText() {
+        if let mediaAltText = modifiedNotificationContent?.userInfo["image_alt"] as? String {
+            if !mediaAltText.isEmpty {
+                modifiedNotificationContent?.body = mediaAltText
+            }
+        }
     }
 }
