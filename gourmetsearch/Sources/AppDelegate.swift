8332c4e61b405c806221db69f2642d94fe7345f4
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-09-09 19:13:04 +0900

    プッシュ通知のデリゲート追加（処理はない）

--- gourmetsearch/Sources/AppDelegate.swift
+++ gourmetsearch/Sources/AppDelegate.swift
@@ -41,6 +41,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+        print(userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
     }
 
